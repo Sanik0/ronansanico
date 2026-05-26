eb117bfc770ec0f611d7a5d6bb1f12d081609bb4
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-05-26 17:35:06 +0800

    poppins font

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         {/* LOGO */}
         <div className={"flex items-center px-15 gap-0"}>
           <h1 className={"text-white font-semibold text-md py-3 px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>SANICORONAN</h1>
-          <p className={"text-gray-400 font-semibold text-xs px-2.5"}>
+          <p className={"text-gray-400 font-medium text-xs px-2.5"}>
             SOFTWARE DEVELOPER
           </p>
         </div>
@@ -38,7 +38,7 @@ export default function Home() {
           <div className={"text-white py-3 hover:bg-white/20 transsition-all cursor-pointer font-medium text-xs px-2.5 border-l-[0.5px]  border-r-[0.5px] border-[#CCCCCC]"}>
             CHECK MY CV
           </div>
-          <div className={"text-gray-400 py-3 hover:bg-gray-400/20 transsition-all cursor-pointer font-medium text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
+          <div className={"text-gray-400 py-3 flex items-center justify-center hover:bg-gray-400/20 transsition-all cursor-pointer font-medium text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
             <span className="material-symbols-outlined text-[24px]">
               arrow_drop_down
             </span>
