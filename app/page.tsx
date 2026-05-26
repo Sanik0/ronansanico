90e30d8205980e14a550761e0bc35f7bcf131f3b
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-05-26 16:48:51 +0800

    google icons added

--- app/page.tsx
+++ app/page.tsx
@@ -15,19 +15,19 @@ export default function Home() {
 
         {/* NAVIGATION LINKS */}
         <ul className={"list-none p-0 m-0 flex items-center justify-center gap-0"}>
-          <li className={"text-white py-3 hover:bg-white/20 transsition-all cursor-pointer font-semibold text-xs px-2.5 border-l-[0.5px]  border-r-[0.5px] border-[#CCCCCC]"}>
+          <li className={"text-white font-medium py-3 hover:bg-white/20 transsition-all cursor-pointer text-xs px-2.5 border-l-[0.5px]  border-r-[0.5px] border-[#CCCCCC]"}>
             ABOUT ME
           </li>
-          <li className={"text-gray-400 py-3 hover:bg-gray-400/20 transsition-all cursor-pointer font-semibold text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
+          <li className={"text-gray-400 font-medium py-3 hover:bg-gray-400/20 transsition-all cursor-pointer text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
             TECH STACK
           </li>
-          <li className={"text-gray-400 py-3 hover:bg-gray-400/20 transsition-all cursor-pointer font-semibold text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
+          <li className={"text-gray-400 font-medium py-3 hover:bg-gray-400/20 transsition-all cursor-pointer text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
             PROJECTS
           </li>
-          <li className={"text-gray-400 py-3 hover:bg-gray-400/20 transsition-all cursor-pointer font-semibold text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
+          <li className={"text-gray-400 font-medium py-3 hover:bg-gray-400/20 transsition-all cursor-pointer text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
             CERTIFICATES
           </li>
-          <li className={"text-gray-400 py-3 hover:bg-gray-400/20 transsition-all cursor-pointer font-semibold text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
+          <li className={"text-gray-400 font-medium py-3 hover:bg-gray-400/20 transsition-all cursor-pointer text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
             TESTIMONIALS
           </li>
 
@@ -35,11 +35,13 @@ export default function Home() {
 
         {/* CV */}
         <div className={"list-none p-0 m-0 flex items-center justify-center gap-0"}>
-          <div className={"text-white py-3 hover:bg-white/20 transsition-all cursor-pointer font-semibold text-xs px-2.5 border-l-[0.5px]  border-r-[0.5px] border-[#CCCCCC]"}>
+          <div className={"text-white py-3 hover:bg-white/20 transsition-all cursor-pointer font-medium text-xs px-2.5 border-l-[0.5px]  border-r-[0.5px] border-[#CCCCCC]"}>
             CHECK MY CV
           </div>
-          <div className={"text-gray-400 py-3 hover:bg-gray-400/20 transsition-all cursor-pointer font-semibold text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
-            TESTIMONIALS
+          <div className={"text-gray-400 py-3 hover:bg-gray-400/20 transsition-all cursor-pointer font-medium text-xs px-2.5 border-r-[0.5px] border-[#CCCCCC]"}>
+            <span className="material-symbols-outlined text-[24px]">
+              arrow_drop_down
+            </span>
           </div>
 
         </div>
