574cd43bb9fc3f21b6982af236a83cb8ade4864a
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-05-27 10:33:57 +0800

    hero section started

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       {/* NAVIGATION */}
       <nav className="w-full px-15 bg-black flex items-center justify-center">
         {/* LOGO */}
-        <div className={"flex w-full items-center px-15 gap-0"}>
+        <div className={"flex w-full items-center justify-start gap-0"}>
           <h1 className={"text-white whitespace-nowrap font-semibold text-md py-3 pl-0 px-2.5 border-r-[0.5px] border-[#CCCCCC]/30"}>SANICORONAN</h1>
           <p className={"textext-[#CCCCCC] whitespace-nowrap font-medium text-xs px-2.5"}>
             SOFTWARE DEVELOPER
@@ -34,7 +34,7 @@ export default function Home() {
         </ul>
 
         {/* CV */}
-        <div className={"list-none w-full p-0 m-0 flex items-center justify-center gap-0"}>
+        <div className={"list-none w-full p-0 m-0 flex items-center justify-end gap-0"}>
           <div className={"flex items-center texttext-[#CCCCCC] font-medium hover:bg-white/20 transition-all py-4 cursor-pointer text-xs px-2.5 border-l-[0.5px] border-[#CCCCCC]/30"}>
             CHECK MY CV
           </div>
@@ -46,6 +46,12 @@ export default function Home() {
 
         </div>
       </nav>
+
+      {/* HERO SECTION */}
+      <div className={"w-full flex items-center justify-center h-133 gap-0"}>
+        <div className={"w-full border-b-[0.5px] border-r-[0.5px] border-black h-full bg-none"}></div>
+        <div className={"w-full max-w-[43%] border-b-[0.5px] border-r-[0.5px] border-black h-full bg-[#FF2D00]"}></div>
+      </div>
     </div>
   );
 }
